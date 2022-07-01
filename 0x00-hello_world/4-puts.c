0dda5f85b137766f4126d6bfa34fd2351ebfcc39
Cherif94800 <cherifadam225@hotmail.fr>
2022-07-01 02:27:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include <stdio.ch>
-
+#include<stdio.h>
+/**
+ * * main -entry point
+ * * Return: Always 0 (sucess)
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
-(return0)
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
