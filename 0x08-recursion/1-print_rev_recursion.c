a773a5540f4accacc9b095d81614f5e2a2ca5892
Cherif94800 <cherifadam225@hotmail.fr>
2022-07-21 16:33:34 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include « main.h »
+
+/**
+ * _print_rev_recursion - imprimer une chaîne à l’envers
+ * @s: chaîne
+ */
+
+vide _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		rendre;
+
+ _print_rev_recursion(s) + 1);
+ _putchar(s) ;
+}
